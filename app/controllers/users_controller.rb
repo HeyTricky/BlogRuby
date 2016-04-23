6b420ae2056a713223cfe6c25a31d047b6b14b13
Natasha <kovnata95@mail.ru>
2016-04-23 15:58:43 +0300

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,46 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-
   def new
+    @user = User.new
+  end
+ 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+      flash[:success] = "Welcome to the blog Fludilka"
+    else
+      render :new
+    end
   end
 
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(user_params)
+    if @user.update(user_params)
+      redirect_to @user
+      flash[:success] = "Ваши данные были успешно изменены"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private 
+    def user_params
+      params.require(:user).permit(:name,:email,:password,:password_confirmation)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
-  has_many :posts, dependent: :destroy
-  has_many :comments, dependent: :destroy
-
+  has_many :posts
+  has_many :comments
   before_save {self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
